f449cd4c17bf2d6ece0a98e73956957db9e5dbe8
김영재 <kim-youngjae@gim-yeongjaeui-MacBookAir.local>
2022-07-20 15:12:25 +0900

    main으로 branch 변경

--- app.js
+++ app.js
@@ -83,3 +83,4 @@ color.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
